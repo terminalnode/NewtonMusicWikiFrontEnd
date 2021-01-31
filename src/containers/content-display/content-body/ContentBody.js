ec3b75c156d9f63b9011bc103a2c5eebddac200a
Alexander Rundberg <alexanderrundberg@fastmail.jp>
2021-01-31 20:15:36 +0100

    add content for home

--- src/containers/content-display/content-body/ContentBody.js
+++ src/containers/content-display/content-body/ContentBody.js
@@ -1,12 +1,19 @@
 import './ContentBody.css';
+import HomePage from '../../../components/contents/home-page/HomePage'
 
 function ContentBody({contentName}) {
   return (
     <div className="ContentBody">
       {/* TODO create a function for displaying content based on content name */}
-      {contentName}
+      {getContent(contentName)}
     </div>
   );
+}
+
+function getContent(contentName) {
+  switch (contentName) {
+    case "home": return <HomePage />
+  }
 }
 
 export default ContentBody;
