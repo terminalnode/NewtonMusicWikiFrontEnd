0ed4bd10440804118dc51c15588d616d73c57feb
SebastianLindfors <sebastianlindfors@gmail.com>
2021-02-02 16:50:43 +0100

    Updated axios call

--- src/apis/artistActions.js
+++ src/apis/artistActions.js
@@ -1,7 +1,15 @@
 import endpoint from './endpoint';
 
 export const getArtistList = async (artistList) => {
+    console.log("Fetching all artists.")
     await endpoint.get("/rest/v1/artist")
-        .then(r => artistList.set(r))
-        .catch(e => console.log(e));
+    .then(resp => {
+        console.log("Retrived Data:", resp.data)
+        artistList.set(resp.data)
+    })
+    .catch(e => console.log("Error:", e));
+
+    // await endpoint.get("/rest/v1/artist")
+    //    .then(r => artistList.set(r))
+    //    .catch(e => console.log(e));
 }
--- src/components/contents/artist-page/all/ArtistAllPage.js
+++ src/components/contents/artist-page/all/ArtistAllPage.js
@@ -2,9 +2,13 @@ import { getArtistList } from '../../../../apis/artistActions';
 import './ArtistAllPage.css';
 
 function ArtistAllPage({artistList}) {
+
   // This isn't really working yet.
-  if (artistList.get == null) {
+  if (artistList.get === "test") {
     getArtistList(artistList);
+  }
+  else {
+    console.log("Updated Artists List",artistList.get);
   }
 
   return (
