f32032913c010640553e6754d7b869d44167d481
Alexander Rundberg <alexanderrundberg@fastmail.jp>
2021-02-07 00:35:28 +0100

    almost implement routing

---
+++ src/components/navigation/NewtonAppBar.js
@@ -0,0 +1,63 @@
+import './NewtonAppBar.css';
+import { AppBar, IconButton, Toolbar, useTheme } from '@material-ui/core';
+import AlbumIcon from '@material-ui/icons/Album';
+import AudiotrackIcon from '@material-ui/icons/Audiotrack';
+import FaceIcon from '@material-ui/icons/Face';
+import GroupIcon from '@material-ui/icons/Group';
+import HomeIcon from '@material-ui/icons/Home';
+import MenuIcon from '@material-ui/icons/Menu';
+import RadioIcon from '@material-ui/icons/Radio';
+import { Link } from "react-router-dom";
+
+// List of all available icons:
+// https://material-ui.com/components/material-icons/
+
+export default function NewtonAppBar() {
+  const classes = useTheme();
+
+  return (
+    <AppBar position="top" className={ classes.appBar }>
+      <Toolbar variant="dense">
+        <IconButton>
+          <MenuIcon />
+        </IconButton>
+
+        <Link to="/" className="Link">
+          <IconButton>
+            <HomeIcon /> Home
+          </IconButton>
+        </Link>
+
+        <Link to="/artists" className="Link">
+          <IconButton>
+            <RadioIcon /> Performers
+          </IconButton>
+        </Link>
+
+        <Link to="/artists/people" className="Link">
+          <IconButton>
+            <FaceIcon /> Artists
+          </IconButton>
+        </Link>
+
+        <Link to="/artists/bands" className="Link">
+          <IconButton>
+            <GroupIcon /> Bands
+          </IconButton>
+        </Link>
+
+        <Link to="/albums" className="Link">
+          <IconButton>
+            <AlbumIcon /> Albums
+          </IconButton>
+        </Link>
+
+        <Link to="/songs" className="Link">
+          <IconButton>
+            <AudiotrackIcon /> Songs
+          </IconButton>
+        </Link>
+      </Toolbar>
+    </AppBar>
+  );
+}
--- src/containers/app/App.js
+++ src/containers/app/App.js
@@ -1,30 +1,24 @@
 import './App.css';
-import { CssBaseline, Grid, ThemeProvider } from '@material-ui/core';
+import { Container, CssBaseline, ThemeProvider } from '@material-ui/core';
 import { useState } from 'react';
-import NewtonAppBar from '../../components/newton-app-bar/NewtonAppBar';
-import ContentDisplay from '../content-display/ContentDisplay';
 import { themeNewton } from './themeNewton';
+import NewtonAppBar from '../../components/navigation/NewtonAppBar';
+import { BrowserRouter as Router } from "react-router-dom";
+import NewtonSwitch from './NewtonSwitch';
 
 function App() {
   console.log("Loaded up and ready!");
-  const [ contentName, setContentName ] = useState("home")
-  const [ artistList, setArtistList ] = useState("test")
-  const [ albumList, setAlbumList ] = useState("test")
-  const [ songList, setSongList ] = useState("test")
-
+  const [ _, setContentName ] = useState("home")
 
   return (
     <ThemeProvider theme={themeNewton}>
-      <CssBaseline />
-      <Grid container>
-        <NewtonAppBar setContentNameFunction={setContentName}/>
-        <ContentDisplay
-          contentName={contentName}
-          artistList={{ get: artistList, set: setArtistList }}
-          albumList={{ get: albumList, set: setAlbumList }}
-          songList={{get: songList, set: setSongList}}
-        />
-      </Grid>
+      <Router>
+        <CssBaseline />
+        <NewtonAppBar />
+        <Container>
+          <NewtonSwitch />
+        </Container>
+      </Router>
     </ThemeProvider>
   );
 }
