7221b8049ce37b3a1fdd2b721071a7e93a2e56e6
Alexander Rundberg <alexanderrundberg@fastmail.jp>
2021-02-07 14:39:46 +0100

    add longitude/latitude to artist list

--- src/components/contents/artist-page/artist-list/ArtistList.js
+++ src/components/contents/artist-page/artist-list/ArtistList.js
@@ -2,10 +2,34 @@ import './ArtistList.css';
 import { DataGrid } from '@material-ui/data-grid';
 
 const columns = [
-  { field: 'id', headerName: 'Id', width: 150 },
-  { field: 'name', headerName: 'Name', width: 150 },
-  { field: 'artistType', headerName: 'Artist type', width: 150 },
+  { field: 'id', headerName: 'Id', width: 80 },
+  { field: 'name', headerName: 'Name', width: 200 },
+  { field: 'type', headerName: 'Type', width: 90 },
+  { field: 'longitude', headerName: 'Longitude', width: 130 },
+  { field: 'latitude', headerName: 'Latitude', width: 130 },
 ];
+
+function mapArtistToRow(artists) {
+  return artists.map(x => {
+    const artist = {};
+    artist.id = x.id;
+    artist.name = x.name;
+    artist.latitude = x.latitude === null ? "n/a" : x.latitude;
+    artist.longitude = x.longitude === null ? "n/a" : x.longitude;
+
+    if (x.artistType === null) {
+      artist.type = "n/a";
+    } else if (x.artistType === "BAND") {
+      artist.type = "Band";
+    } else if (x.artistType === "PERSON") {
+      artist.type = "Person";
+    } else {
+      artist.type = x.artistType;
+    }
+
+    return artist;
+  })
+}
 
 export default function ArtistList({artists}) {
   return (
@@ -14,7 +38,7 @@ export default function ArtistList({artists}) {
       checkboxSelection
       color="primary"
       columns={ columns }
-      rows={ artists }
+      rows={ mapArtistToRow(artists) }
       pageSize={ 10 }
     />
   );
