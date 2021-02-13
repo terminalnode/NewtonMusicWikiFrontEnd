3c234465b498f8b3af1f6f8a7523e8874617390d
SebastianLindfors <sebastianlindfors@gmail.com>
2021-02-13 16:29:39 +0100

    Centered the modal. Finally.

--- src/components/newton-modal/NewtonModal.js
+++ src/components/newton-modal/NewtonModal.js
@@ -3,8 +3,13 @@ import { Backdrop, Box, Container, makeStyles, Modal, Typography } from "@materi
 const useStyles = makeStyles((theme) => ({
     modal: {
         display: 'flex',
+        margin: 'auto',
         alignItems: 'center',
+        background: 'grey',
         justifyContent: 'center',
+        maxHeight: '50%',
+        maxWidth: '50%',
+        //transform: 'translate(50%, 50%)'
     }
 }));
 
