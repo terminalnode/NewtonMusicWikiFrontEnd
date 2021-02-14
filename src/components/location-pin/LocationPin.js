a3707371b555bc16ba762b50bd86fa2fe77c84d9
Alexander Rundberg <alexanderrundberg@fastmail.jp>
2021-02-14 14:18:49 +0100

    remove failed attempt at location pin

--- src/components/location-pin/LocationPin.js
+++
@@ -1,9 +0,0 @@
-import { locationPinStyle } from './locationPinStyle';
-
-export default function MyGreatPlace(props) {
-  return (
-    <div style={ locationPinStyle }>
-      {props.text}
-    </div>
-  );
-}
